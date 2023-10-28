9a38862723d3e35c1876815aa23ec675ff669616
Moaz Elsafty <moaz.elsafty11@gmail.com>
2023-10-28 11:59:32 +0200

    More info about the project in README. Adjusting some functinos and adding functionalit.

--- server/envelopes.js
+++ server/envelopes.js
@@ -1,41 +1,74 @@
 const express = require('express');
 const envelopesRouter = express.Router();
-const { findEnvelopeById, updateSpecificEnvelope } = require('./helper')
+const {
+    findEnvelopeByCategory,
+    updateSpecificEnvelope,
+    chooseCategory,
+    createEnvelope,
+    transferBudget
+} = require('./helper')
 
-let envelopeIdCounter = 1
 
 let envelopes = [];
 
 //Creaete a new envelope
 envelopesRouter.post('/', (req, res, next) => {
-    let newEnvelope = {
-        id: envelopeIdCounter++,
-        budget: `${req.query.budget}$`,
-        title: req.query.title
+    const category = chooseCategory(req.query.type)
+    const budget = req.query.budget
+    const newEnvelope = createEnvelope(category, budget);
+    if (newEnvelope) {
+        envelopes.push(newEnvelope);
+        res.status(201).send(`A new ${category} envelope successfully created`);
+    } else {
+        // incase user missed or misspelled the requirments
+        res.status(404).send(`Invalid information. Please, checkout your inputs craiteria.`) //<<<<
     }
-    envelopes.push(newEnvelope);
-    res.status(201).send('New envelope successfully created');
 })
 
 //get an array of all envelopes
 envelopesRouter.get('/', (req, res, next) => {
-    res.status(200).json(envelopes);
+    res.status(200).send(envelopes);
 });
 
-//get an envelope by id
-envelopesRouter.get('/:envelopeId', (req, res, next) => {
-    const id = req.params.envelopeId;
-    const requiredEnvelope = findEnvelopeById(envelopes, id);
+//get an envelope by category
+envelopesRouter.get('/:envelopeCategory', (req, res, next) => {
+    const category = req.params.envelopeCategory;
+    const requiredEnvelope = findEnvelopeByCategory(envelopes, category);
     res.status(200).send(requiredEnvelope);
 })
 
 //Update a specific envelope
-envelopesRouter.put('/:envelopeId', (req, res, next) => {
-    const id = req.params.envelopeId;
-    let specificEnvelope = findEnvelopeById(envelopes, id);
+envelopesRouter.put('/:envelopeCategory', (req, res, next) => {
+    const category = req.params.envelopeCategory;
+    let specificEnvelope = findEnvelopeByCategory(envelopes, category);
     const updatedEnvelope = updateSpecificEnvelope(specificEnvelope, req.query.budget)
-    res.status(200).send(`Envelope with id: ${id} has been updated successfully`)
+    res.status(200).send(`${category} Envelope has been updated successfully`)
 })
 
+//Transfer budgets from different envelopes
+envelopesRouter.post('/transfer/:from/:to', (req, res, next) => {
+    const transferFrom = req.params.from;
+    console.log(transferFrom)
+    const transferTo = req.params.to;
+    console.log(transferTo)
+    const envelope1 = findEnvelopeByCategory(envelopes, transferFrom)
+    const envelope2 = findEnvelopeByCategory(envelopes, transferTo)
+    const test = transferBudget(envelope1, envelope2);
+    res.status(200).send(`Transfering budget operation done`)
+})
+
+
+//delete a specific envelope
+envelopesRouter.delete('/:deletedEnvelove', (req, res, next) => {
+    const deletedEnveloveCategory = req.params.deletedEnvelove;
+    const envelopeToDeleteCategory= findEnvelopeByCategory(envelopes, deletedEnveloveCategory);
+    if(envelopeToDeleteCategory) {
+        const index = envelopes.findIndex((ele) => ele.category == envelopeToDeleteCategory)
+        envelopes.splice(index, 1)
+        res.status(204).send(`${deletedEnveloveCategory} envelope has been deleted successfully`);
+    } else {
+        res.status(500);
+    }
+})
 
 module.exports = envelopesRouter;
--- server/helper.js
+++ server/helper.js
@@ -1,15 +1,55 @@
 
-const findEnvelopeById = (arr, id) => {
-    id = Number(id);
-    if (id) {
-        return arr.find((ele) => ele.id === id) || `Not valid id: ${id}`
+const chooseCategory = (type) => {
+    switch(type.toLowerCase()){
+        case 'groceries':
+            return 'Groceries';
+        case 'gas':
+            return 'Gas';
+        case 'clothing':
+            return 'Clothing';
+        case 'tax':
+            return 'Tax';
+    }
+}
+
+const createEnvelope = (category, budget) => {
+    if(category && budget) {
+        return {
+            category: category,
+            budget: budget
+        }
+    } 
+}
+
+const findEnvelopeByCategory = (arr, category) => {
+    const wantedEnvelope = category;
+    if (category) {
+        const requiredEnvelope = arr.find((ele) => ele.category.toLowerCase() == wantedEnvelope.toLowerCase())
+        if (requiredEnvelope) {
+            return requiredEnvelope;
+        } else {
+            return `Invalid ${category} category.`
+        }
     } else {
-        return "Id required"
+        return "Category Required"
     }
 };
+
+const transferBudget = (envelope1, envelope2) => {
+    console.log(envelope1.budget)
+    console.log( envelope2.budget)
+    envelope2.budget = envelope1.budget
+    return envelope2;
+}
 
 const updateSpecificEnvelope = (envelope, updates) => {
     return envelope.budget = updates;
 }
 
-module.exports = { findEnvelopeById , updateSpecificEnvelope };
+module.exports = {
+    findEnvelopeByCategory,
+    updateSpecificEnvelope,
+    chooseCategory,
+    createEnvelope,
+    transferBudget
+};
